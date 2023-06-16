02ae4cf06294ebc20ec235f967fe669a251068a0
Antoine <antoine1.gajan@gmail.com>
2023-06-16 09:24:57 +0200

    Ajout de vérifications liées au menu de la fenetre

--- fenetre.py
+++ fenetre.py
@@ -15,24 +15,27 @@ class MessengerStatistics (tkinter.Tk):
 
     def create_menu(self):
         """Crée le menu de la fenêtre"""
-        menu_bar = tkinter.Menu(self)
+        self.menu_bar = tkinter.Menu(self)
         # Premier sous-menu
-        menu1 = tkinter.Menu(menu_bar, tearoff = 0)
-        menu1.add_command(label = "Ouvrir un fichier", command = self.open_file)
-        menu1.add_command(label = "Ouvrir un dossier", command = self.open_folder)
-        menu1.add_command(label = "Quitter", command = self.destroy)
-        menu_bar.add_cascade(label = "Fichier", menu = menu1)
+        self.menu1 = tkinter.Menu(self.menu_bar, tearoff = 0)
+        self.menu1.add_command(label = "Ouvrir un fichier", command = self.open_file)
+        self.menu1.add_command(label = "Ouvrir un dossier", command = self.open_folder)
+        self.menu1.add_command(label = "Quitter", command = self.destroy)
+        self.menu_bar.add_cascade(label = "Fichier", menu = self.menu1)
 
         # Deuxième sous-menu
-        menu2 = tkinter.Menu(menu_bar, tearoff = 0)
-        menu2.add("command", label = "Général", command = lambda: self.show_global_stats())
-        menu2.add_command(label = "Messages par jour", command = lambda: self.show_messages_by_day())
-        menu2.add_command(label = "Messages par mois", command = lambda: self.show_messages_by_month())
-        menu2.add_command(label = "Messages par année", command = lambda: self.show_messages_by_year())
-        menu2.add_command(label = "Conversations les plus actives", command = lambda: self.show_most_active_conversation())
-        menu_bar.add_cascade(label = "Graphiques", menu = menu2)
+        self.menu2 = tkinter.Menu(self.menu_bar, tearoff = 0)
+        self.menu2.add("command", label = "Général", command = lambda: self.show_global_stats())
+        self.menu2.add_command(label = "Messages par jour", command = lambda: self.show_messages_by_day())
+        self.menu2.add_command(label = "Messages par mois", command = lambda: self.show_messages_by_month())
+        self.menu2.add_command(label = "Messages par année", command = lambda: self.show_messages_by_year())
+        self.menu2.add_command(label = "Conversations les plus actives", command = lambda: self.show_most_active_conversation())
+        self.menu_bar.add_cascade(label = "Graphiques", menu = self.menu2)
         # Configuration du menu
-        self.config(menu = menu_bar)
+        self.config(menu = self.menu_bar)
+
+        # Désactivation du menu
+        self.disable_menu()
 
 
     def get_username(self):
@@ -42,13 +45,11 @@ class MessengerStatistics (tkinter.Tk):
     def open_folder(self):
         """Ouvre un dossier et crée le dictionnaire des messages"""
         folder = filedialog.askdirectory()
-        print(folder)
         self.create_dict_messages(folder)
 
     def open_file(self):
         """Ouvre un fichier et crée le dictionnaire des messages"""
         file = filedialog.askopenfilename(title = "Select a file", filetypes = (("json files", "*.json"), ("all files", "*.*")))
-        print(file)
         self.set_dict_messages(file)
 
     def create_dict_messages(self, folder: str):
@@ -60,18 +61,18 @@ class MessengerStatistics (tkinter.Tk):
     def set_dict_messages(self, file: str):
         """Crée le dictionnaire des messages à partir d'un fichier"""
         self.dict_messages = json.load(open(file, "r"))
-        print("Dict messages set")
         self.create_images()
 
     def create_images(self):
         """Crée les images des graphiques"""
-        print(self.dict_messages)
         # Create images directory
         os.makedirs("images", exist_ok=True)
         graph.draw_messages_by_day(self.dict_messages)
         graph.draw_messages_by_month(self.dict_messages)
         graph.draw_messages_by_year(self.dict_messages)
         graph.draw_most_active_conversation(self.dict_messages, 10, True, True)
+        # Activation du menu
+        self.enable_menu()
 
     def show_image(self, image_path: str):
         """Affiche une image dont le chemin est passé en paramètre"""
@@ -89,7 +90,7 @@ class MessengerStatistics (tkinter.Tk):
         self.labels.append(label)
         label.pack()
         # Informations du compte
-        label_info_compte = tkinter.Label(self, text = "Informations du compte", font = ("Arial", 16, "bold"))
+        label_info_compte = tkinter.Label(self, text = "Informations du compte", font = ("Arial", 16, "bold", "underline"))
         self.labels.append(label_info_compte)
         label_info_compte.pack(padx = 20, pady=20, side= TOP, anchor="w")
         label_nom = tkinter.Label(self, text = "Nom: " + self.get_username())
@@ -105,7 +106,7 @@ class MessengerStatistics (tkinter.Tk):
         self.labels.append(label_nb_messages)
         label_nb_messages.pack(pady=10, side= TOP, anchor="w")
         # Statistiques par conversation
-        label_subtitle_stats = tkinter.Label(self, text = "Statistiques par conversation", font = ("Arial", 16, "bold"))
+        label_subtitle_stats = tkinter.Label(self, text = "Statistiques par conversation", font = ("Arial", 16, "bold", "underline"))
         self.labels.append(label_subtitle_stats)
         label_subtitle_stats.pack(padx = 20, pady=20, side= TOP, anchor="w")
         label_nb_avg_messages_per_conversation = tkinter.Label(self, text = "Nombre moyen de messages par conversation: " + str(self.dict_messages["user"]["nb_avg_messages_per_conversation"]))
@@ -162,4 +163,14 @@ class MessengerStatistics (tkinter.Tk):
             label.destroy()
         # Nettoyer la liste
         self.labels.clear()
+
+    def enable_menu(self):
+        """Fonction qui active le menu"""
+        self.menu_bar.entryconfig("Graphiques", state="normal")
+
+    def disable_menu(self):
+        """Fonction qui désactive le menu"""
+        self.menu_bar.entryconfig("Graphiques", state="disabled")
+
+        
 
